8ce91f8daacdc96c3d51047a24eaadab9a7de0bb
gdownes <gdownes@gmail.com>
2014-05-28 11:31:43 +0100

    First commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+﻿$(function () {
+    var count = 0;
+    $(".counter").fitText(0.4);
+    $(".button").fitText(0.3);
+    $("#countValue").text(count);
+    $("#plus").on("click", function() {
+        count = count + 1;
+        $("#countValue").text(count);
+    });
+    $("#minus").on("click", function () {
+        count = count - 1;
+        $("#countValue").text(count);
+    });
+    document.body.onmousedown = function () { return false; }
+});
