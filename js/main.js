7e0d8212e8fb0346595fac1d4a266ed849ac7644
unknown <GDownes@BFS-DOTNET-3.kana.com>
2014-05-29 09:46:07 +0100

    Reworking reset button

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 ﻿$(function () {
     var count = 0;
-    $(".counter").fitText(0.4);
-    $(".button").fitText(0.3);
+    //$(".counter").fitText(0.4);
+    //$(".button").fitText(0.3);
     $("#countValue").text(count);
     $("#plus").on("click", function() {
         count = count + 1;
